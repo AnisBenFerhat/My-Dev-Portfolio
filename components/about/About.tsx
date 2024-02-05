e53dc3251541c4ac28ebbc86e14fbedb993884c8
AnisBenFerhat <anisbenferhat@gmail.com>
2024-02-05 18:25:40 +0100

    Fix(About): Replace Tabs style to Accordion style
    - Enhance the layout
    - Reorganise the data, with separation of concerns for better readability

--- components/about/About.tsx
+++ components/about/About.tsx
@@ -1,59 +1,36 @@
-import { techIcons } from '@/utils/Data';
+// About.tsx
+
 import { BiCode } from 'react-icons/bi';
-import { Tabs, TabsList, TabsTrigger } from '../ui/Tabs';
+import AboutAccordion from './AboutAccordion';
 
 const About = () => {
-  const categories = Array.from(new Set(techIcons.map((icon) => icon.category)));
-
   return (
-    <section id='about' className='xl:h-[860px] pb-12 py-6 xl:py-24'>
+    <section id='about' className='pb-12 py-6 xl:py-24'>
       <div className='container mx-auto'>
         <h2 className='section-title mb-12 xl:mb-24 text-center mx-auto'>
           <BiCode className='inline-block text-4xl mr-2' /> About me
         </h2>
 
-        <div className='grid grid-cols-1 lg:grid-cols-2 gap-8'>
-          {/* Left Cell for Presentation */}
-          <div className='xl:col-span-1 subtitle tracking-wide leading-[26px] border-secondary-10/50 px-4 xl:border-r-2'>
-            <p className='mb-6'>
-              Hey there, I'm Anis, a Paris-based frontend developer with a unique journey. I've spent over +15 years in
-              retail, honing my skills in management, customer relations, and coaching. This diverse experience has
-              equipped me with valuable insights into user experiences and effective team collaboration.
-            </p>
-            <p className='mb-6'>
-              Alongside my retail journey, I delved into naturopathy and Shiatsu, exploring the intricate connections
-              between well-being and technology. Now, I'm on a mission to fuse my rich background with my passion for
-              programming to craft meaningful and user-centric digital experiences.
-            </p>
-            <p>
-              I thrive on the problem-solving challenges that coding brings. Eager to embrace new technologies, I'm
-              currently seeking opportunities to contribute to innovative projects as a frontend developer.
-            </p>
-          </div>
-
-          {/* Border (Visible on Medium and Smaller Screens) */}
-          <div className='xl:hidden border-b border-secondary-30/50 my-8 w-full'></div>
-
-          {/* Right Cell */}
-          <div className='xl:col-span-1 flex justify-center '>
-            {/* Tabs Component */}
-            <Tabs>
-              <TabsList className='bg-secondary-70/80 text-primary-10'>
-                <TabsTrigger className='hover:bg-secondary-10/50 ' value='tech-skill'>
-                  Tech Stacks
-                </TabsTrigger>
-                <TabsTrigger className='hover:bg-secondary-10/50 ' value='soft-skill'>
-                  Soft Skills
-                </TabsTrigger>
-                <TabsTrigger className='hover:bg-secondary-10/50 ' value='other-skill'>
-                  More to know
-                </TabsTrigger>
-              </TabsList>
-            </Tabs>
-          </div>
+        <div className='border-b border-secondary-10/50 mb-8 max-w-screen-lg mx-auto px-4'>
+          {' '}
+          <p className='subtitle tracking-wide leading-[26px]'>
+            Hey there, I'm Anis, a Paris-based frontend developer with a unique journey. I've spent over +15 years in
+            retail, honing my skills in management, customer relations, and coaching. This diverse experience has
+            equipped me with valuable insights into user experiences and effective team collaboration.
+          </p>
+          <p className='subtitle tracking-wide leading-[26px]'>
+            Alongside my retail journey, I delved into naturopathy and Shiatsu, exploring the intricate connections
+            between well-being and technology. Now, I'm on a mission to fuse my rich background with my passion for
+            programming to craft meaningful and user-centric digital experiences.
+          </p>
+          <p className='subtitle tracking-wide leading-[26px]'>
+            I thrive on the problem-solving challenges that coding brings. Eager to embrace new technologies, I'm
+            currently seeking opportunities to contribute to innovative projects as a frontend developer.
+          </p>
         </div>
 
-        {/* Rest of the Section */}
+        {/* Render the AboutAccordion component */}
+        <AboutAccordion />
       </div>
     </section>
   );
---
+++ components/about/AboutAccordion.tsx
@@ -0,0 +1,140 @@
+// AboutAccordion.tsx
+
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../ui/Accordion';
+import { softSkills, techIcons, alsoData } from '@/utils/Data';
+import { SoftSkillProps } from '@/utils/Types';
+
+const AboutAccordion = () => {
+  const categories = Array.from(new Set(techIcons.map((icon) => icon.category)));
+
+  return (
+    <>
+      {/* Accordion for Tech Stacks */}
+      <Accordion type='single' collapsible className='max-w-screen-lg mx-auto' defaultValue='expertise'>
+        <AccordionItem value='expertise'>
+          <AccordionTrigger>Programming Expertise</AccordionTrigger>
+          <AccordionContent>
+            {/* Tech Icons Content */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-4'>
+              {categories.map((category) => (
+                <div key={category} className='mb-4'>
+                  <h3 className='font-semibold mb-2'>{category}</h3>
+                  <div className='grid grid-cols-8 gap-2'>
+                    {techIcons
+                      .filter((icon) => icon.category === category)
+                      .map((icon) => (
+                        <div key={icon.name} className='flex items-center space-x-2'>
+                          <img src={icon.path} alt={icon.name} className='w-6 h-6 object-contain' />
+                        </div>
+                      ))}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      {/* Accordion for Soft Skills */}
+      <Accordion type='single' collapsible className='max-w-screen-lg mx-auto'>
+        <AccordionItem value='soft-skills'>
+          <AccordionTrigger>Soft Skills</AccordionTrigger>
+          <AccordionContent>
+            <ul className='grid xl:grid-cols-3 md:grid-cols-2 gap-2 list-none p-0'>
+              {softSkills.map((softSkill: SoftSkillProps) => (
+                <li key={softSkill} className='py-1 px-2 rounded-full mb-2'>
+                  {softSkill}
+                </li>
+              ))}
+            </ul>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+
+      {/* Accordion for Other Skills */}
+      <Accordion type='single' collapsible className='max-w-screen-lg mx-auto'>
+        <AccordionItem value='also'>
+          <AccordionTrigger>Also...</AccordionTrigger>
+          <AccordionContent>
+            {/* Language Skills */}
+            <div className='mb-8'>
+              <h3 className='text-lg mb-2'>{alsoData.languages.title}</h3>
+              {/* Iterate over subcategories */}
+              <div className='grid xl:grid-cols-3 gap-4'>
+                {Object.keys(alsoData.languages.icons).map((subcategory) => (
+                  <div key={subcategory} className='mb-4'>
+                    <h4 className='mb-2 capitalize'>{subcategory}</h4>
+                    <div className='grid grid-cols-6 gap-4'>
+                      {/* Iterate over icons within subcategory */}
+                      {Object.keys(alsoData.languages.icons[subcategory]).map((key) => (
+                        <div key={key} className='flex items-center space-x-2'>
+                          <img
+                            src={alsoData.languages.icons[subcategory][key]}
+                            alt={key}
+                            className='w-6 h-6 object-contain'
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* Learning */}
+            <div className='mb-8'>
+              <h3 className='text-lg mb-2'>{alsoData.learning.title}</h3>
+              {/* Iterate over subcategories */}
+              <div className='grid xl:grid-cols-3 gap-4'>
+                {Object.keys(alsoData.learning.icons).map((subcategory) => (
+                  <div key={subcategory} className='mb-4'>
+                    <h4 className='mb-2 capitalize'>{subcategory}</h4>
+                    <div className='grid grid-cols-6 gap-4'>
+                      {/* Iterate over icons within subcategory */}
+                      {Object.keys(alsoData.learning.icons[subcategory]).map((key) => (
+                        <div key={key} className='flex items-center space-x-2'>
+                          <img
+                            src={alsoData.learning.icons[subcategory][key]}
+                            alt={key}
+                            className='w-6 h-6 object-contain'
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* Hobbies */}
+            <div className='mb-8'>
+              <h3 className='text-lg mb-2'>{alsoData.hobbies.title}</h3>
+              {/* Iterate over subcategories */}
+              <div className='grid xl:grid-cols-3 gap-4'>
+                {Object.keys(alsoData.hobbies.icons).map((subcategory) => (
+                  <div key={subcategory} className='mb-4'>
+                    <h4 className='mb-2 capitalize'>{subcategory}</h4>
+                    <div className='grid grid-cols-6 gap-4'>
+                      {/* Iterate over icons within subcategory */}
+                      {Object.keys(alsoData.hobbies.icons[subcategory]).map((key) => (
+                        <div key={key} className='flex items-center space-x-2'>
+                          <img
+                            src={alsoData.hobbies.icons[subcategory][key]}
+                            alt={key}
+                            className='w-6 h-6 object-contain'
+                          />
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </>
+  );
+};
+
+export default AboutAccordion;
