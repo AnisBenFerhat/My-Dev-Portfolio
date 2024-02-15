9c3dfa58396e80951aabe6ea075e6563235e0149
AnisBenFerhat <anisbenferhat@gmail.com>
2024-02-15 12:25:53 +0100

    Fix(Hero): Increase position of Chevron Icon

--- components/hero/Hero.tsx
+++ components/hero/Hero.tsx
@@ -45,7 +45,7 @@ const Hero = () => {
               </div>
             </div>
           </div>
-          <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-20 animate-pulse xl:animate-bounce '>
+          <div className='hidden md:flex absolute left-2/4 bottom-20 xl:bottom-10 animate-pulse xl:animate-bounce '>
             <BsChevronCompactDown className='text-3xl text-secondary-80 mb-24' />
           </div>
         </div>
